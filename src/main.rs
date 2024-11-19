1c4ec89ed38ea45ea8059331a5f3e9864d8307f6
Tharos <dubois.siegfried@gmail.com>
2024-11-19 11:07:33 +0100

    CLI update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod index_gfa_file;
-use std::env;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version = "v0.1.0", about, long_about = None)]
+struct Cli {
+    /// The path to the GFA file
+    file_path: String,
+    /// Extract only the paths with the names given in the file.
+    #[arg(short = 'r', long = "rename")]
+    names_file: Option<String>,
+}
 
 fn main() {
     /*
@@ -11,12 +21,10 @@ fn main() {
     It will print to standard information about the paths
     */
     // Get the file path from command line arguments
-    let args: Vec<String> = env::args().collect();
-    let file_path: &String = &args[1];
-    let path_names_file: Option<&String> = args.get(2);
+    let args: Cli = Cli::parse();
 
-    let path_name_vector: Vec<String> = match path_names_file {
-        Some(path_names_file) => match index_gfa_file::read_pathlist_file(path_names_file) {
+    let path_name_vector: Vec<String> = match args.names_file {
+        Some(path_names_file) => match index_gfa_file::read_pathlist_file(&path_names_file) {
             Ok(result) => result,
             Err(error) => {
                 eprintln!("Failed to read path names file: {}", error);
@@ -26,7 +34,7 @@ fn main() {
         None => Vec::new(),
     };
 
-    let () = match index_gfa_file::reconstruct_paths(file_path, path_name_vector) {
+    let () = match index_gfa_file::reconstruct_paths(&args.file_path, path_name_vector) {
         Ok(result) => result,
         Err(error) => {
             eprintln!("Failed to read GFA file: {}", error);
